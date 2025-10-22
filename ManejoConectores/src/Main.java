e0da74e1fd274244b5cac76a2a61cefa1310e879
Mohammed Bachiri Jabbouri <mohammedbachiri17@gmail.com>
2025-10-22 17:17:23 +0200

    Añadiendo cambios

--- ManejoConectores/src/Main.java
+++ ManejoConectores/src/Main.java
@@ -1,8 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.sql.*;
-
-
 public class Main {
     public static void main(String[] args) {
         try {
@@ -10,17 +8,17 @@ public class Main {
             //falta cargar el driver ya.
             // Class.forName("com.mysql.jdbc.Driver");
             // Establecemos la conexion con la BD
-            Connection conexion =
-            DriverManager.getConnection("jdbc:mysql://localhost/manejo_conectores", "root", "");
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/manejo_conectores", "root",
+                            "");
             // Preparamos la consulta
             Statement sentencia = conexion.createStatement();
             String sql = "SELECT * FROM departamentos";
             ResultSet resul = sentencia.executeQuery(sql);
             // Recorremos el resultado para visualizar cada fila
-                // Se hace un bucle mientras haya registros y se van visualizando
+            // Se hace un bucle mientras haya registros y se van visualizando
             while (resul.next()) {
                 System.out.printf("%d, %s, %s %n", resul.getInt(1),
-                        resul.getString(2), resul.getString(3));
+                resul.getString(2), resul.getString(3));
             }
             resul.close(); // Cerrar ResultSet
             sentencia.close(); // Cerrar Statement
